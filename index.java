b10d56296692bbe12a9e93d760d678fdd022ac0a
Dillyganesh <dillymaxwell@gmail.com>
2023-07-03 13:03:06 +0530

    intro java first program

---
+++ index.java
@@ -0,0 +1,12 @@
+class hey{
+	public static void main(String args[]){
+		System.out.println("hello world");
+	}
+}
+
+
+class hello{
+	 void main(String args[]){
+		System.out.println("Iam back");
+	}
+}
